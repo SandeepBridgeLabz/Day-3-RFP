6a553ff7f18384f6a15840eb9db32716bd0abc42
SandeepBridgeLabz <sandeep.mishra@bridgelabz.com>
2026-05-14 15:20:16 +0530

    Added UC4 Switch Case

--- App/src/EmployeeWageComputation.java
+++ App/src/EmployeeWageComputation.java
@@ -8,14 +8,21 @@ public class EmployeeWageComputation {
 
         int empHrs = 0;
 
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        if (empCheck == IS_PART_TIME)
-            empHrs = 4;
-        else if (empCheck == IS_FULL_TIME)
-            empHrs = 8;
-        else
-            empHrs = 0;
+        switch (empCheck) {
+
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+
+            default:
+                empHrs = 0;
+        }
 
         int empWage = empHrs * EMP_RATE_PER_HOUR;
 
